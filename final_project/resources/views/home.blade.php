27dc8e27113ed5b3a098021c6cb0bcedad31dbe1
Alexander Shyshkov <shisha_90@list.ru>
2018-06-18 17:56:38 +0300

    Corrections

--- final_project/resources/views/home.blade.php
+++ final_project/resources/views/home.blade.php
@@ -4,6 +4,7 @@
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="description" content="Photofilters - web application for change your photos!" />
         <title>{{ config('app.name', 'Photofilters') }}</title>
 		<link href="{{ asset('css/filter.css') }}" rel="stylesheet"/>
 		<link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet"/>
